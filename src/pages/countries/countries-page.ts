53f54224132bd8173834679a17068910ade76326
Claudio Quiroz <claudioquirozipi@gmail.com>
2025-01-27 13:01:09 -0500

    add flags

--- src/pages/countries/countries-page.ts
+++ src/pages/countries/countries-page.ts
@@ -13,7 +13,10 @@ export class CountriesPage extends LitElement {
   elementController = new ElementController(this);
   apiUrl: string = 'https://restcountries.com/v3.1/all';
 
+  countries:any[] = []
+
   async fetchCountries() {
+    console.log("mundo")
     try {
         const response = await fetch(this.apiUrl);
         if (!response.ok) {
@@ -21,6 +24,7 @@ export class CountriesPage extends LitElement {
         }
         const countries = await response.json();
         console.log({countries});
+        return countries;
     } catch (error) {
         console.error('Error:', error);
     }
@@ -30,14 +34,52 @@ export class CountriesPage extends LitElement {
     
   }
 
+  async onPageEnter(){
+    console.log("hola")
+    const countriesResponse = await this.fetchCountries()
+    console.log({countriesResponse})
+    this.countries = countriesResponse;
+    this.requestUpdate()
+  }
+
   static styles = css`
     h1 {
       color: red;
+    }
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+      gap: 1.5rem;
+    }
+
+    .item {
+
+    }
+    p {
+      text-align: center;
+      margin: 0;
+    }
+    img {
+      width: 100%;
+      height: 100px;
+      object-fit: contain;
     }
 `
   render() {
     return html`
     <h1>Paises</h1>
+
+    <div class="grid">
+      ${this.countries.map((country: any)=>(html`<div class="item">
+        
+        <img src="${country.flags.png}" />
+        <p>
+
+          ${country.name.common}
+        </p>
+      
+      </div>`))}
+    </div>
 
     `;
   }
